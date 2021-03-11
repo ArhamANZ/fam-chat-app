42c33c39b16fc296f1638118f7875bf867b43d70
Arham Zahedy <arhamanz152@gmail.com>
2021-03-11 13:56:25 +0600

    First Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# fam_chat_app
+
+A fully featured chat app with Flutter and Firebase including Android, IOS, Web and Desktop implementation.
---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:fam_chat_app/views/signin.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.red,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: SignIn(),
+    );
+  }
+}
